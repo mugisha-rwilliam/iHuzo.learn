6be36884fcc6b83fd1a1e812604fd7b044370567
mugisha-rwilliam <137517138+mugisha-rwilliam@users.noreply.github.com>
2024-04-11 15:55:15 +0200

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,70 @@
 alert('Hello! Welcome to Baia');
+/*
+const names = ['shaun', 'mario', 'luigi'];
+let i = 5;
+do{
+  console.log('val of i is: ', i);
+  i++;
+}
+while(i < names.length);
+
+const age = 20;
+
+if(age > 20){
+  console.log('you are over 20 years old')
+}
+
+
+const password = "passdf@dssword 124";
+
+if (password.length >= 12 && password.includes("@")) {
+  console.log("that password is mighty strong");
+} else if(password.length >= 8 || password.includes("@")) {
+  console.log('that password is strong enough!');
+} else {
+  console.log("password is not strong enough");
+}
+
+
+console.log(!true);
+
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for (let i = 0; i < scores.length; i++) {
+  
+  if(scores[i] === 0){
+    continue;
+  }
+  console.log('your score: ', scores[i]);
+  
+  if (scores[i] === 100){
+    console.log("congrats, you got the top score!")
+    break;
+    
+  }
+}
+
+
+const grade = "50"
+
+switch(grade){
+  case '50':
+    console.log("you got an A!");
+    break;
+  case 'B':
+    console.log("you got an B!");
+    break;
+  case 'C':
+    console.log("you got an C!");
+    break;
+  case 'D':
+    console.log("you got an D!");
+    break;
+  case 'E':
+    console.log("you got an E!");
+    break;
+  default:
+    console.log("not a valid grade");
+}
+
+*/
