b64c90a24c6514cacba16cfa8b521aa618b439f6
mugisha-rwilliam <137517138+mugisha-rwilliam@users.noreply.github.com>
2024-04-17 12:10:09 +0200

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -67,4 +67,75 @@ switch(grade){
     console.log("not a valid grade");
 }
 
+const speak = function (){
+  console.log ("Good day");
+};
+
+function greet(){
+  console.log ("Hello there");
+}
+
+greet()
+speak ()
+
+
+const speak = function(name = 'luigi', time = 'night'){
+  console.log('good ${time} ${name}');
+};
+
+speak();
+
+
+
+const calcArea = function(radius){
+  let area = 3.14 * radius**2;
+  console.log (area);
+  return area;
+}
+
+const calcArea = function(radius){
+  return 3.14 * radius**2;
+};
+
+
+const calcArea = radius =>{
+  return 3.14 * radius**2;
+};
+
+
+const calcArea = radius => 3.14 * radius**2;
+};
+
+
+const area = calcArea(5);
+console.log("area is:", area);
+
+
+const greet = () => "hello world";
+
+const result = greet();
+console.log(result);
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] * tax;
+    
+  }
+  return total;
+};
+
+console.log(bill([10, 15, 30], 0.2));
+
+const greet = () => "hello";
+
+let resultOne = greet();
+console.log(resultOne);
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
+
 */
+
+
+
