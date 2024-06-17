c95bc0901c5173959d6034d6e42397afa1c6d6e4
mugisha-rwilliam <137517138+mugisha-rwilliam@users.noreply.github.com>
2024-06-17 23:00:09 +0200

    Update main.java with number validators

--- main.java
+++ main.java
@@ -6,14 +6,25 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
         final float MAX_INTEREST_RATE = 20.0f; // Maximum allowed interest rate
+        int principal = 1000;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        float principal = scanner.nextFloat();
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal < 1000 || principal > 1000000) {
+                System.out.print("The principal should be more than 1,000 and not higher than 1,000,000. Please enter a valid number: ");
+                continue;
+            }
+            else {
+                if (principal > 1000 && principal < 1000000)
+                break;
+            }
+        }
 
         // Input validation for annual interest rate
-        float annualInterest;
+        float annualInterest = 10;
         while (true) {
             System.out.print("Annual Interest Rate (%): ");
             annualInterest = scanner.nextFloat();
@@ -24,9 +35,16 @@ public class Main {
             }
         }
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        
+        byte years = 5;
+        
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            if (years >= 5 && years <= 30)
+                break;
+            System.out.println("Enter any number between 4 and 31: ");
+        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal
